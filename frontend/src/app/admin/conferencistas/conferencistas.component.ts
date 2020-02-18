fbd6e474f2c7a9a5dbf9fc9f1f3f1688bc7b50a4
Juan Guerra <juanguerra97@protonmail.com>
2020-02-18 15:37:07 -0600

    Pantalla simple de conferencistas

--- frontend/src/app/admin/conferencistas/conferencistas.component.ts
+++ frontend/src/app/admin/conferencistas/conferencistas.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlus, faTrashAlt, faEdit, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { IConferencista } from '../../models/IConferencista';
+import { ConferencistaService } from '../../services/conferencista.service';
 
 @Component({
   selector: 'app-conferencistas',
@@ -7,9 +12,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConferencistasComponent implements OnInit {
 
-  constructor() { }
+  faPlus = faPlus;
+  faEdit = faEdit;
+  faTrashAlt = faTrashAlt;
+  faSave = faSave;
+  faTimes = faTimes;
+
+  public opcionNuevoConf = false;
+  public conferencistas: IConferencista[] = [];
+  public selectedConf: IConferencista = null;
+
+  public guardando = false;
+
+  public formConf = new FormGroup({
+    nombre: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(64)
+    ]),
+    profesion: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(128)
+    ]),
+    resumen: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(256)
+    ]),
+  });
+
+  constructor(
+    private confService: ConferencistaService,
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit() {
+    this.confService.getAll()
+      .subscribe(res => {
+        this.conferencistas = res;
+      }, console.error);
+  }
+
+  public onSubmitFormConf(): void {
+
+    if (!this.formConf.valid) return;
+    this.guardando = true;
+
+    if (this.opcionNuevoConf) {
+      this.confService.create(this.formConf.value)
+        .subscribe(res => {
+
+          this.guardando = false;
+          this.modalService.dismissAll();
+
+        }, console.error);
+    } else {
+
+      this.guardando = false;
+
+    }
+
+  }
+
+  public onEliminarConf(): void {
+    if (this.selectedConf == null) return;
+  }
+
+  public onConfClicked(conferencista: IConferencista): void {
+    if (this.selectedConf == null || this.selectedConf.id !== conferencista.id) {
+      this.selectedConf = conferencista;
+    } else {
+      this.selectedConf = null;
+    }
+  }
+
+  public openModalNuevo(content): void {
+    this.opcionNuevoConf = true;
+    this.formConf.reset();
+    this.openModal(content);
+  }
+
+  public openModalEditar(content): void {
+    if (this.selectedConf == null) return;
+    this.opcionNuevoConf = false;
+    this.formConf.reset(this.selectedConf);
+    this.openModal(content);
+  }
+
+  public openModal(content): void {
+    this.modalService.open(content, {
+      size: 'lg',
+      centered: true,
+      windowClass: 'animated bounceIn'
+    });
   }
 
 }
