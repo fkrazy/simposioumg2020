b6a6fc321a367e622dedccb871342fee6f9d80ed
Juan Guerra <juanguerra97@protonmail.com>
2020-02-18 13:51:23 -0600

    Service para conferencistas

---
+++ frontend/src/app/services/conferencista.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IConferencista } from '../models/IConferencista';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConferencistaService{
+
+  static BASE_URL = 'assets/mock';
+
+  private conferencistas: IConferencista[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get<IConferencista[]>(`${ConferencistaService.BASE_URL}/conferencistas.json`)
+      .subscribe(res => {
+        this.conferencistas = res;
+      });
+  }
+
+  public getAll(): Observable<IConferencista[]> {
+    return of(this.conferencistas);
+  }
+
+  public create(conferencista: IConferencista): Observable<number> {
+    const id = 1000 + this.conferencistas.length + 1;
+    conferencista.id = id;
+    this.conferencistas.push(conferencista);
+    return of(id);
+  }
+
+}
