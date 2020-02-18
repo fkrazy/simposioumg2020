43a7d3062ce4c70ba29069785e033c4b52fc5e00
Juan Guerra <juanguerra97@protonmail.com>
2020-02-18 13:55:46 -0600

    Servicio para salones

---
+++ frontend/src/app/services/salon.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ISalon } from '../models/ISalon';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalonService {
+
+  static BASE_URL = 'assets/mock';
+
+  private salones: ISalon[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get<ISalon[]>(`${SalonService.BASE_URL}/salones.json`)
+      .subscribe(res => {
+        this.salones = res;
+      });
+  }
+
+  public getAll(): Observable<ISalon[]> {
+    return of(this.salones);
+  }
+
+  public create(salon: ISalon): Observable<number> {
+    const id = 2200 + this.salones.length + 1;
+    salon.id = id;
+    this.salones.push(salon);
+    return of(id);
+  }
+
+}
