62d1b193ad0a0fee34193c24a21965b24d014c0d
Juan Guerra <juanguerra97@protonmail.com>
2020-02-19 12:24:52 -0600

    Pantalla simple para que los asistentes registren su pago

--- frontend/src/app/asistentes/pago/pago.component.ts
+++ frontend/src/app/asistentes/pago/pago.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { faEdit, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ICuenta } from '../../models/ICuenta';
+import { CuentaService } from '../../services/cuenta.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-pago',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagoComponent implements OnInit {
 
-  constructor() { }
+  faEdit = faEdit;
+  faSave = faSave;
+  faTimes = faTimes;
+
+  public cuentas: ICuenta[] = [];
+
+  public registrandoPago = false;
+
+  public formRegistroPago = new FormGroup({
+    foto: new FormControl('', [
+      Validators.required,
+    ])
+  });
+
+  constructor(
+    private cuentaService: CuentaService,
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit() {
+    this.cuentaService.getAll().subscribe((res) => {
+      this.cuentas = res;
+    }, console.error);
+  }
+
+  public onSubmitFormRegistroPago(): void {
+
+  }
+
+  public openModalRegistroPago(content): void {
+    this.modalService.open(content, {
+      size: 'lg',
+      centered: true,
+      windowClass: 'animated bounceIn'
+    });
   }
 
 }
