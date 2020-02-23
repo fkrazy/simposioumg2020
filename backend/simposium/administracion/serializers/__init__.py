d95ef8574b87e881e04f58f1bf8175d39371c7cf
una mirada cientifica <f2kloa@gmail.com>
2020-02-23 16:06:47 -0600

    create crud conferencias

--- backend/simposium/administracion/serializers/__init__.py
+++ backend/simposium/administracion/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .asistencias import AsistenciaSerializer
 from .asistentes import AsistenteSerializer
 from .carreras import CarreraSerializer
+from .conferencias import ConferenciaSerializer
--- backend/simposium/administracion/urls.py
+++ backend/simposium/administracion/urls.py
@@ -4,9 +4,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'', views.AsistenciasViewSet)
-router.register(r'', views.AsistenteViewSet)
-router.register(r'', views.CarreraViewSet)
+router.register(r'asistencias', views.AsistenciasViewSet)
+router.register(r'asistentes', views.AsistenteViewSet)
+router.register(r'carreras', views.CarreraViewSet)
+router.register(r'conferencias', views.ConferenciaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
