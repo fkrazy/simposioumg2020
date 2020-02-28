07c5a3a901863a93b26798db64df9bccfff766ad
Juan Guerra <juanguerra97@protonmail.com>
2020-02-27 20:36:21 -0600

    Unique=True para el nombre de carrera

--- backend/simposium/administracion/models/asistentes.py
+++ backend/simposium/administracion/models/asistentes.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Asistente(models.Model):
-    usuario = models.ForeignKey(User, related_name="Asistente", verbose_name="Usuario", on_delete=models.CASCADE)
+    usuario = models.OneToOneField(User, related_name="Asistente", verbose_name="Usuario", on_delete=models.CASCADE)
     telefono = models.CharField(max_length=20, null=False)
     first_login = models.BooleanField()
--- backend/simposium/administracion/models/carreras.py
+++ backend/simposium/administracion/models/carreras.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Carrera(models.Model):
-    nombre = models.CharField(max_length=100, null=False)
+    nombre = models.CharField(max_length=100, null=False, unique=True)
 
     def __str__(self):
         return self.nombre
