314bf229ad98879ca3f5d14c88f48b6074ec9b40
Juan Guerra <juanguerra97@protonmail.com>
2020-03-02 15:54:58 -0600

    renombramiento de numero_cuenta a numero en el modelo Cuenta

--- backend/simposium/administracion/models/cuentas.py
+++ backend/simposium/administracion/models/cuentas.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Cuenta(models.Model):
-    numero_cuenta = models.CharField(max_length=30, verbose_name="Número de cuenta")
+    numero = models.CharField(max_length=30, verbose_name="Número de cuenta")
     banco = models.CharField(max_length=30, verbose_name="Banco")
     titular = models.CharField(max_length=30, verbose_name='Titular')
 
     class Meta:
-        unique_together = [['numero_cuenta','banco']]
+        unique_together = [['numero','banco']]
 
     def __str__(self):
-        return self.numero_cuenta
+        return self.numero
