2f23ea9469b411bc96cecc1759c58b9c40e87869
Juan Guerra <juanguerra97@protonmail.com>
2020-03-02 15:13:28 -0600

    Se agrega el codigo de carrera al modelo

--- backend/simposium/administracion/models/carreras.py
+++ backend/simposium/administracion/models/carreras.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Carrera(models.Model):
+    codigo = models.PositiveIntegerField(null=False, unique=True)
     nombre = models.CharField(max_length=100, null=False, unique=True)
 
     def __str__(self):
