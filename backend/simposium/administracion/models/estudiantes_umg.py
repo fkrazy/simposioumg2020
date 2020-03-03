e89997ee984eda852142ee0556dc2d1e00e629d1
Juan Guerra <juanguerra97@protonmail.com>
2020-03-02 18:46:33 -0600

    Correcciones

--- backend/simposium/administracion/models/estudiantes_umg.py
+++ backend/simposium/administracion/models/estudiantes_umg.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 from .carreras import Carrera
+from .asistentes import Asistente
 
 
 class EstudianteUmg(models.Model):
-    usuario = models.ForeignKey(User, related_name="EstudianteUmg", verbose_name="Usuario", on_delete=models.CASCADE)
-    carnet = models.CharField(max_length=25, null=False)
-    semestre = models.SmallIntegerField(null=False)
-    codigo_carrera = models.ForeignKey(Carrera, related_name="EstudianteUmg", verbose_name="Carrera", on_delete=models.CASCADE)
+    asistente = models.OneToOneField(Asistente, related_name="EstudianteUmg", verbose_name="Asistente", on_delete=models.CASCADE)
+    carnet = models.CharField(max_length=25, null=False, unique=True)
+    semestre = models.PositiveSmallIntegerField(null=False, verbose_name='Semestre')
+    carrera = models.ForeignKey(Carrera, related_name="estudiantes", verbose_name="Carrera", on_delete=models.PROTECT)
