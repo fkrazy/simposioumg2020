c17757d4a524529fa8efa0fb511ed48df89f7fd4
Juan Guerra <juanguerra97@protonmail.com>
2020-03-04 17:33:41 -0600

    se agrega permiso al viewset de pagos y endpoints para devolver pagos pendientes y en solicitud de reembolso

--- backend/simposium/administracion/views/pagos.py
+++ backend/simposium/administracion/views/pagos.py
@@ -1,9 +1,50 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from ..models import Pago
 from ..serializers import PagoSerializer
+from ..permisos import PermisoPagos
 
 
 class PagoViewSet(viewsets.ModelViewSet):
     serializer_class = PagoSerializer
     queryset = Pago.objects.all()
+    permission_classes = [PermisoPagos]
+
+    def filter_queryset(self, queryset):
+        for g in self.request.user.groups.all():
+            if g.name == 'ASISTENTE':
+                return queryset.filter(titular=self.request.user.asistente)
+        return queryset
+
+
+    # devuelve los pagos pendientes de validacion
+    @action(detail=False, methods=['GET'])
+    def pendientes(self, request):
+        pendientes_validacion = self.filter_queryset(self.get_queryset())\
+            .filter(estado=Pago.PENDIENTE_VALIDACION)\
+            .order_by('fecha_registro')
+
+        page = self.paginate_queryset(pendientes_validacion)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(pendientes_validacion, many=True)
+        return Response(serializer.data)
+
+
+    # devuelve los pagos que han solicitado reembolso
+    @action(detail=False, methods=['GET'])
+    def reembolsos(self, request):
+        solicitud_reembolsos = self.filter_queryset(self.get_queryset()).filter(estado=Pago.EVALUACION_REEMBOLSO)
+
+        page = self.paginate_queryset(solicitud_reembolsos)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(solicitud_reembolsos, many=True)
+        return Response(serializer.data)
+
