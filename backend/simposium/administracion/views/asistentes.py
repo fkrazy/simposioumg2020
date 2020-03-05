e463b820a83ead013ae5abeb164c8a56e0ac419e
Juan Guerra <juanguerra97@protonmail.com>
2020-03-05 15:44:47 -0600

    custom retrieve method para devolver un asistente

--- backend/simposium/administracion/views/asistentes.py
+++ backend/simposium/administracion/views/asistentes.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from ..models import Asistente
 from ..serializers import AsistenteSerializer
@@ -7,3 +8,17 @@ from ..serializers import AsistenteSerializer
 class AsistenteViewSet(viewsets.ModelViewSet):
     serializer_class = AsistenteSerializer
     queryset = Asistente.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        # serializer = self.get_serializer(instance)
+        return Response({
+            "usuario": {
+                "id": instance.usuario.id,
+                "nombres": instance.usuario.first_name,
+                "apellidos": instance.usuario.last_name,
+                "username": instance.usuario.username,
+                "correo": instance.usuario.email
+            },
+            "telefono": instance.telefono
+        })
