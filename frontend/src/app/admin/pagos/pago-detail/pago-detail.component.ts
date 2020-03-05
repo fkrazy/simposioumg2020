cf8c003130ebce487033df42f94e3192fc164d14
Juan Guerra <juanguerra97@protonmail.com>
2020-03-05 14:03:23 -0600

    creacion component PagoDetail

---
+++ frontend/src/app/admin/pagos/pago-detail/pago-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pago-detail',
+  templateUrl: './pago-detail.component.html',
+  styleUrls: ['./pago-detail.component.scss']
+})
+export class PagoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- frontend/src/app/admin/pagos/pagos.module.ts
+++ frontend/src/app/admin/pagos/pagos.module.ts
@@ -6,6 +6,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PagosComponent } from './pagos/pagos.component';
 import { ValidacionesPagosComponent } from './validaciones-pagos/validaciones-pagos.component';
 import { ReembolsosPagosComponent } from './reembolsos-pagos/reembolsos-pagos.component';
+import { PagoDetailComponent } from './pago-detail/pago-detail.component';
 
 const routes: Route[] = [
   {
@@ -20,11 +21,14 @@ const routes: Route[] = [
     path: 'reembolsos',
     component: ReembolsosPagosComponent
   },
-
+  {
+    path: ':id',
+    component: PagoDetailComponent
+  },
 ];
 
 @NgModule({
-  declarations: [PagosComponent, ValidacionesPagosComponent, ReembolsosPagosComponent],
+  declarations: [PagosComponent, ValidacionesPagosComponent, ReembolsosPagosComponent, PagoDetailComponent],
   imports: [
     CommonModule,
     FormsModule,
