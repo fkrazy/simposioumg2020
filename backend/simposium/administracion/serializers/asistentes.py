df677a82806cee88fdc6e24643188cb36adada59
Juan Guerra <juanguerra97@protonmail.com>
2020-03-08 11:45:00 -0600

    serializer para leer un asistente

--- backend/simposium/administracion/serializers/asistentes.py
+++ backend/simposium/administracion/serializers/asistentes.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
 from ..models import Asistente
+from ..serializers.usuarios import ReadUserSerializer
 
 
 class AsistenteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asistente
         fields = '__all__'
+
+
+class ReadAsistenteSerializer(serializers.ModelSerializer):
+
+    usuario = ReadUserSerializer()
+
+    class Meta:
+        model = Asistente
+        fields = ['usuario', 'telefono']
+        read_only_fields = ['usuario', 'telefono']
