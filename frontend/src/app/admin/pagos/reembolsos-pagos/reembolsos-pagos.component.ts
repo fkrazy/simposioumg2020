2a9407eb554dc69569c1b381644a0e45bf3a5119
Juan Guerra <juanguerra97@protonmail.com>
2020-03-09 12:54:28 -0600

    se muestran los pagos cuyo reembolso ha sido aprobado y los pagos que ya han sido reembolsados

--- frontend/src/app/admin/pagos/reembolsos-pagos/reembolsos-pagos.component.ts
+++ frontend/src/app/admin/pagos/reembolsos-pagos/reembolsos-pagos.component.ts
@@ -13,6 +13,8 @@ export class ReembolsosPagosComponent implements OnInit {
   API_URL = environment.apiUrl;
 
   private pagosReembolsoPendiente: IPago[] = [];
+  private pagosReembolsoAprobado: IPago[] = [];
+  private pagosReembolsados: IPago[] = [];
 
   constructor(
     private pagoService: PagoService
@@ -20,11 +22,25 @@ export class ReembolsosPagosComponent implements OnInit {
 
   ngOnInit() {
     this.cargarPagosPendientesReembolso();
+    this.cargarPagosReembolsoAprobado();
+    this.cargarPagosReembolsados();
   }
 
   private cargarPagosPendientesReembolso(): void {
     this.pagoService.getAllReembolsosPendientes().subscribe((res) => {
       this.pagosReembolsoPendiente = res;
+    }, console.error);
+  }
+
+  private cargarPagosReembolsoAprobado(): void {
+    this.pagoService.getAllReembolsosAprobados().subscribe((res) => {
+      this.pagosReembolsoAprobado = res;
+    }, console.error);
+  }
+
+  private cargarPagosReembolsados(): void {
+    this.pagoService.getAllReembolsados().subscribe((res) => {
+      this.pagosReembolsados = res;
     }, console.error);
   }
 
