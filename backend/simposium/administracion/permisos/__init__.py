02e7be9ff679f14469ceaf01182d6768416383a4
Juan Guerra <juanguerra97@protonmail.com>
2020-03-10 14:18:47 -0600

    Permiso para el viewset de evaluaciones de reembolso

--- backend/simposium/administracion/permisos/__init__.py
+++ backend/simposium/administracion/permisos/__init__.py
@@ -2,3 +2,4 @@ from .commons import EsAdminOrReadOnly
 from .pagos import PermisoPagos
 from .validacion_pago import PermisoValidacionesPago
 from .tickets import PermisoTickets
+from .evaluacion_reembolso import PermisoEvaluacionesReembolso
--- backend/simposium/administracion/views/evaluacion_reembolso.py
+++ backend/simposium/administracion/views/evaluacion_reembolso.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 
 from ..models import EvaluacionReembolso
 from ..serializers import EvaluacionReembolsoSerializer
+from ..permisos import PermisoEvaluacionesReembolso
 
 
 class EvaluacionReembolsoViewSet(viewsets.ModelViewSet):
     serializer_class = EvaluacionReembolsoSerializer
     queryset = EvaluacionReembolso.objects.all()
+    permission_classes = [PermisoEvaluacionesReembolso]
