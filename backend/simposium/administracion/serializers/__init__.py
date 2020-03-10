e13c25ee25b4cb7ce23dfab486473c43559f5719
Juan Guerra <juanguerra97@protonmail.com>
2020-03-10 11:59:09 -0600

    se importa ReadAsistenteSerializer

--- backend/simposium/administracion/serializers/__init__.py
+++ backend/simposium/administracion/serializers/__init__.py
@@ -1,5 +1,5 @@
 from .asistencias import AsistenciaSerializer
-from .asistentes import AsistenteSerializer
+from .asistentes import AsistenteSerializer, ReadAsistenteSerializer
 from .carreras import CarreraSerializer
 from .conferencias import ConferenciaSerializer
 from .conferencistas import ConferencistaSerializer
