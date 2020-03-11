74b3f52371f8780a73430cb75054349805a67252
Juan Guerra <juanguerra97@protonmail.com>
2020-03-10 21:14:49 -0600

    se sobreescribio el metodo get_serializer_class

--- backend/simposium/administracion/views/conferencias.py
+++ backend/simposium/administracion/views/conferencias.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions
+from rest_framework.permissions import SAFE_METHODS
+
 from ..models import Conferencia
-from ..serializers import ConferenciaSerializer
+from ..serializers import ConferenciaSerializer, ReadConferenciaSerializer
 from ..permisos.commons import EsAdminOrReadOnly
 
 
@@ -8,3 +10,8 @@ class ConferenciaViewSet(viewsets.ModelViewSet):
     serializer_class = ConferenciaSerializer
     queryset = Conferencia.objects.all()
     permission_classes = [EsAdminOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return ReadConferenciaSerializer
+        return ConferenciaSerializer
