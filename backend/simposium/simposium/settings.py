78c0cdfe042ab1d70a716f43ff9a06414731be57
Juan Guerra <juanguerra97@protonmail.com>
2020-03-10 21:16:12 -0600

    se establecen los valores de MEDIA_URL y MEDIA_ROOT

--- backend/simposium/simposium/settings.py
+++ backend/simposium/simposium/settings.py
@@ -142,6 +142,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL =  '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 try:
     from .local_settings import *
 except ImportError:
