8b75c170f4bbc1586b9aeddfdbd1c66e96cf8041
Juan Guerra <juanguerra97@protonmail.com>
2020-03-18 15:36:51 -0600

    numero de depositos realizados a cada cuenta

--- backend/simposium/administracion/serializers/cuentas.py
+++ backend/simposium/administracion/serializers/cuentas.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
-
-from ..models import Cuenta
+from django.db.models import Sum, Count
+from ..models import Cuenta, Pago
 
 
 class CuentaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cuenta
         fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+
+        rep["numero_depositos"] = Pago.objects.filter(cuenta=instance).filter(estado=Pago.ACEPTADO).count()
+        return rep
--- frontend/src/app/models/ICuenta.ts
+++ frontend/src/app/models/ICuenta.ts
@@ -4,4 +4,5 @@ export interface ICuenta {
   numero: string;
   banco: string;
   titular: string;
+  numero_depositos?: number;
 }
