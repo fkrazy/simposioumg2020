2b73119820397852051c68063550f576c5c889e4
una mirada cientifica <f2kloa@gmail.com>
2020-03-23 23:28:27 -0600

    show conferencias and send for validation at ticket

--- backend/simposium/validacion/views.py
+++ backend/simposium/validacion/views.py
@@ -1,15 +1,37 @@
 from django.contrib.auth.models import User
 
 # Create your views here.
-from rest_framework import status
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from administracion.models import Ticket
+
 
 class GetValidation(APIView):
+
     def get(self, request, format=None):
-        users = User.objects.all()
-        return Response({})
+        params = request.query_params
+        ticket = Ticket.objects.filter(id=params.get('id'))
+        user = ticket.asistente.usuario
+        if user:
+            if user.password == params.get('ticket'):
+                if not ticket.asistencias.any(conferencia_id=params.get('conf')):
+                    return Response({
+                        "message": "correcto",
+                        "usuario": {
+                            "id": user.id,
+                            "nombres": user.first_name,
+                            "apellidos": user.last_name,
+                            "username": user.username,
+                            "correo": user.email
+                        }
+                    })
+                else:
+                    return Response({'message': "ticket ya ingresado a conferencia"})
+            else:
+                return Response({'message': "qr invalido"})
+        raise Http404
     #
     # def post(self, request, format=None):
     #     serializer = Serializer(data=request.data)
