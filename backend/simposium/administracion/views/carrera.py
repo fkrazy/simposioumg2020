d01b66f099430a7de5d43f3fff3af90617bb206a
Juan Guerra <juanguerra97@protonmail.com>
2020-03-25 11:10:12 -0600

    se sobreescribio en metodo destroy() para capturar las excepciones del tipo ProtectedError y devolver un mensaje mas legible

--- backend/simposium/administracion/views/carrera.py
+++ backend/simposium/administracion/views/carrera.py
@@ -1,11 +1,28 @@
-from rest_framework import viewsets
+from django.db.models import ProtectedError
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from ..models import Carrera
 from ..serializers import CarreraSerializer
 
 from ..permisos import EsAdminOrReadOnly
 
+class CustomModelViewSet(viewsets.ModelViewSet):
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, args, kwargs)
+        except ProtectedError as err:
+            return Response({"detail": str(err)}, status=status.HTTP_400_BAD_REQUEST )
+
+
 class CarreraViewSet(viewsets.ModelViewSet):
     serializer_class = CarreraSerializer
     queryset = Carrera.objects.all()
     permission_classes = [EsAdminOrReadOnly]
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, args, kwargs)
+        except ProtectedError:
+            return Response({"detail": 'Otros registros referencian a esta carrera'}, status=status.HTTP_400_BAD_REQUEST )
--- backend/simposium/administracion/views/salones.py
+++ backend/simposium/administracion/views/salones.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.db.models import ProtectedError
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from ..models import Salon
 from ..serializers import SalonSerializer
@@ -9,3 +11,9 @@ class SalonViewSet(viewsets.ModelViewSet):
     serializer_class = SalonSerializer
     queryset = Salon.objects.all()
     permission_classes = [EsAdminOrReadOnly]
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, args, kwargs)
+        except ProtectedError:
+            return Response({"detail": 'Otros registros referencian a este salon'}, status=status.HTTP_400_BAD_REQUEST )
