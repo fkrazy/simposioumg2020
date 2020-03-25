596a377db16afa1aa4520d8750459b583a23a1e9
Juan Guerra <juanguerra97@protonmail.com>
2020-03-25 10:14:07 -0600

    se usa splitbutton para mostrar usuario logueado y el menu para administradores

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {  faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import {AuthService} from '../auth/auth.service';
+import { AuthService } from '../auth/auth.service';
+import { MenuItem } from 'primeng';
 
 @Component({
   selector: 'app-header',
@@ -9,16 +10,36 @@ import {AuthService} from '../auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
+  public itemsMenuUser: MenuItem[] = [];
+  public itemsMenuAdmin: MenuItem[] = [];
+
   faSignInAlt = faSignInAlt;
-  faSignOutAlt = faSignOutAlt;
 
   isExpanded = false;
 
   constructor(
     private auth: AuthService
-  ) { }
+  ) {
+    this.itemsMenuAdmin = [
+      {label: 'Pagos', routerLink: '/admin/pagos'},
+      {label: 'Validaciones', routerLink: '/admin/pagos/validaciones'},
+      {label: 'Reembolsos', routerLink: '/admin/pagos/reembolsos'},
+      {label: 'Conferencias', routerLink: '/admin/conferencias'},
+      {label: 'Salones', routerLink: '/admin/salones'},
+      {label: 'Conferencistas', routerLink: '/admin/conferencistas'},
+      {label: 'Carreras', routerLink: '/admin/carreras'},
+      {label: 'Cuentas', routerLink: '/admin/cuentas'},
+      {separator: true},
+      {label: 'Salir', icon: 'pi pi-sign-out', routerLink: `/logout`},
+    ];
+
+    this.itemsMenuUser = [
+      {label: 'Salir', icon: 'pi pi-sign-out', routerLink: `/logout`},
+    ];
+  }
 
   ngOnInit() {
+
   }
 
   collapse() {
