8389d4c4176d86cfab6ef20bc0e367255eb982a2
Juan Guerra <juanguerra97@protonmail.com>
2020-03-25 16:56:53 -0600

    se retorna fecha de la forma 25/03/2020, al modificar la fecha de un pago tambien se puede recibir la fecha en esta forma

--- backend/simposium/administracion/serializers/evaluacion_reembolso.py
+++ backend/simposium/administracion/serializers/evaluacion_reembolso.py
@@ -1,9 +1,19 @@
+from datetime import datetime, date
+
+
 from rest_framework import serializers
+from pytz import timezone
 
 from ..models import EvaluacionReembolso
 
+guatemala = timezone('America/Guatemala')
 
 class EvaluacionReembolsoSerializer(serializers.ModelSerializer):
     class Meta:
         model = EvaluacionReembolso
         fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["fecha_hora"] = datetime.strftime(instance.fecha_hora.astimezone(guatemala), "%d/%m/%Y %H:%M:%S")
+        return rep
